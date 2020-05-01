1d84a6654afcf235bcc25ed7906df16c0c20623e
sahar <hoseinisahar999@gmail.com>
2020-05-02 01:29:56 +0430

    -Hierarchy -Message service pilot versions

--- server/src/test/java/tech/builtrix/FileParserTest.java
+++ server/src/test/java/tech/builtrix/FileParserTest.java
@@ -52,7 +52,7 @@ public class FileParserTest {
             // demo : "id": "fae0c9a2-ef89-477a-a073-a9e704e5ccb3",
             // franklin "id": "9d94dd4d-b789-4717-bdee-517a8de8ca6e",
             // parede "id": "4f9e5bc1-471d-4b37-87ca-82f803898bb6",
-            String buildingId = "fae0c9a2-ef89-477a-a073-a9e704e5ccb3";
+            String buildingId = "9d94dd4d-b789-4717-bdee-517a8de8ca6e";
             // deleteAllBuildingsBills(buildingId);
             parseFiles(buildingId);
 
@@ -65,21 +65,9 @@ public class FileParserTest {
     private void parseFiles(String buildingId) {
         String bucket = "metrics-building";
         List<String> documents = Arrays.asList(
-                "fae0c9a2-ef89-477a-a073-a9e704e5ccb3-2020-1.pdf",
-                "fae0c9a2-ef89-477a-a073-a9e704e5ccb3-2020-2.pdf",
-                "fae0c9a2-ef89-477a-a073-a9e704e5ccb3-2020-3.pdf",
-                "fae0c9a2-ef89-477a-a073-a9e704e5ccb3-2019-1.pdf",
-                "fae0c9a2-ef89-477a-a073-a9e704e5ccb3-2019-10.pdf",
-                "fae0c9a2-ef89-477a-a073-a9e704e5ccb3-2019-11.pdf",
-                "fae0c9a2-ef89-477a-a073-a9e704e5ccb3-2019-12.pdf",
-                "fae0c9a2-ef89-477a-a073-a9e704e5ccb3-2019-2.pdf",
-                "fae0c9a2-ef89-477a-a073-a9e704e5ccb3-2019-3.pdf",
-                "fae0c9a2-ef89-477a-a073-a9e704e5ccb3-2019-4.pdf",
-                "fae0c9a2-ef89-477a-a073-a9e704e5ccb3-2019-5.pdf",
-                "fae0c9a2-ef89-477a-a073-a9e704e5ccb3-2019-6.pdf",
-                "fae0c9a2-ef89-477a-a073-a9e704e5ccb3-2019-7.pdf",
-                "fae0c9a2-ef89-477a-a073-a9e704e5ccb3-2019-8.pdf",
-                "fae0c9a2-ef89-477a-a073-a9e704e5ccb3-2019-9.pdf"
+                "11190000431226.pdf", "11190000573173.pdf",
+                "11190000477527.pdf", "11200000001443.pdf",
+                "11190000525021.pdf"
         );
         for (String document : documents) {
             BillDto billDto;
@@ -104,4 +92,5 @@ public class FileParserTest {
             billService.delete(bill.getId());
         }
     }
+
 }
