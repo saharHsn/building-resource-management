70aade3ef8d1923672e66d30a42be890925bb472
sahar <hoseinisahar999@gmail.com>
2020-05-02 02:04:21 +0430

    - complete overall.component

--- angular8-springboot-client/src/app/charts/overall/overall.component.ts
+++ angular8-springboot-client/src/app/charts/overall/overall.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ChartService } from '../chartService';
-import { CurrentMonthSummary } from './CurrentMonthSummary';
-import { User } from 'src/app/_models';
-import { Building } from 'src/app/building/model/building';
-import { BuildingService } from 'src/app/building/service/building.service';
-import { AuthenticationService } from 'src/app/_services';
-import { first, map } from 'rxjs/operators';
-import { of, Observable } from 'rxjs/';
-import { predict } from 'src/app/building/model/predict';
-import { IdServiceService } from 'src/app/_services/id-service.service';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ChartService} from '../chartService';
+import {CurrentMonthSummary} from './CurrentMonthSummary';
+import {User} from 'src/app/_models';
+import {Building} from 'src/app/building/model/building';
+import {BuildingService} from 'src/app/building/service/building.service';
+import {AuthenticationService} from 'src/app/_services';
+import {IdServiceService} from 'src/app/_services/id-service.service';
+
 @Component({
   templateUrl: './overall.component.html',
   styleUrls: ['./overall.component.css']
@@ -20,40 +18,35 @@ export class OverallComponent implements OnInit {
   currentUser: User;
   building: Building = new Building();
 
-
-
-
-
   // gauge variables
 
   beScore: number;
   nationalMedian: number;
   propertyTarget: number;
   buildings: any;
-  constructor(private route: ActivatedRoute,
-    private router: Router,
-    private chartService: ChartService,
-    private buildingService: BuildingService,
-    private authService: AuthenticationService,
-    private idservice:IdServiceService) {
-    this.currentUser = this.authService.currentUserValue;
-    
 
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private chartService: ChartService,
+              private buildingService: BuildingService,
+              private authService: AuthenticationService,
+              private idservice: IdServiceService,
+              // private prediction: PredictionsComponent
+  ) {
+    this.currentUser = this.authService.currentUserValue;
   }
 
   ngOnInit() {
-
     this.getBuildingUsers();
-    //create charts
+    // create charts
     this.initCharts();
-
+    // this.prediction.ngOnInit();
   }
-
-
 
   list() {
     this.router.navigate(['users']);
   }
+
   reloadData() {
     this.buildingService.getBuildingByOwner(this.currentUser).subscribe(
       data => {
@@ -64,41 +57,29 @@ export class OverallComponent implements OnInit {
   }
 
   getBuildingUsers() {
-
     this.buildingService.getBuildingUsersTest().subscribe(
       data => {
-
         this.buildings = data.content;
-
-
-        if(localStorage.getItem('idBuilding')!==null){
+        if (this.idservice.getIdbuilding() !== null) {
           return;
-        }
-        else{
-          let id = data.content[0].id;
+        } else {
+          const id = data.content[0].id;
           this.idservice.setIdbuilding(id);
         }
-       
       },
-
-
       error => console.log(error)
-
     );
   }
-
 
 // item selected
   select(event) {
     this.idservice.setIdbuilding(event);
     this.initCharts();
+    // call prediction ngOnInit
+    // call BeScoreComponent ngOnInit
   }
 
-
-
-
   initCharts() {
-  
     this.id = this.route.snapshot.params.id;
     this.currentMonthSummary = new CurrentMonthSummary();
     this.reloadData();
@@ -107,7 +88,6 @@ export class OverallComponent implements OnInit {
         this.currentMonthSummary.consumption = data.content.consumption;
         this.currentMonthSummary.cost = data.content.cost;
         this.currentMonthSummary.environmental = data.content.environmental;
-
       }, error => console.log(error));
     this.chartService.getNationalMedian()
       .subscribe(data => {
@@ -117,15 +97,5 @@ export class OverallComponent implements OnInit {
       .subscribe(data => {
         this.propertyTarget = data.content;
       }, error => console.log(error));
-
-
-
-
-
-
   }
-
-
-
-
 }
